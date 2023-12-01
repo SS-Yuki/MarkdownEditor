e72dc5d6e0602254d243782dacf591a15b54acb4
YuKang <kangy.me@gmail.com>
2023-12-01 17:34:39 +0800

    finish test

--- test/test.sh
+++ test/test.sh
@@ -10,21 +10,40 @@ for i in {1..5}; do
     input_file="${i}.in"
     output_file="${i}.out"
     answer_file="${i}.ans"
+    create_file="test${i}.md"
+    curr_file="test${i}.ans"
+
     $program < $input_file > $output_file
+
+    # 时间戳特殊处理
     if [ $i -eq 5 ]; then
         cp "$output_file" "${output_file}.copy"
         cp "$answer_file" "${answer_file}.copy"
         sed -i '' '10,11d' "$output_file"
         sed -i '' '10,11d' "$answer_file"
     fi
+
     diff "$output_file" "$answer_file" > /dev/null
     if [ $? -eq 0 ]; then
-        echo "Group $i: Output matches the answer."
+        echo "Test $i: Output Right."
     else
-        echo "Group $i: Output does not match the answer."
+        echo "Test $i: Output Wrong!"
     fi
+
     if [ $i -eq 5 ]; then
         mv "${output_file}.copy" "$output_file"
         mv "${answer_file}.copy" "$answer_file"
     fi
 done
+
+for i in {1..5}; do
+    diff "$create_file" "$curr_file" > /dev/null
+    if [ $? -eq 0 ]; then
+        echo "Test $i: Create .md Right."
+    else
+        echo "Test $i: Create .md Wrong!"
+    fi
+done
+
+
+    
